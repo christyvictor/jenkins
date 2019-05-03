32bdfdd086dedaab9a1dd77bcf7fd169fe868c0f
kvaddemgunta <kvaddemgunta@apple.com>
2019-05-04 01:49:33 +0530

    test added2

--- lab5/src/test/java/ca/umo/csd/cs2212/kristy/TestBankAccount.java
+++ lab5/src/test/java/ca/umo/csd/cs2212/kristy/TestBankAccount.java
@@ -10,6 +10,6 @@ public class TestBankAccount {
   public void testDebitWithSufficientFunds() {
     BankAccount account = new BankAccount(10);
     double amount = account.debit(5);
-    Assert.assertEquals(5,0, amount);
+    Assert.assertEquals(5.0, amount);
    }
 }
